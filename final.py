8b6f811fccbd21a31933583c714783b79944ad93
rohan-singhvi <67252780+rohan-singhvi@users.noreply.github.com>
2023-12-16 12:16:59 -0500

    fixed the optimization and graph generation, with additions of new origin destination randomization + newer constraints

--- final.py
+++ final.py
@@ -2,8 +2,9 @@ import gurobipy as gp
 from gurobipy import GRB
 import pandas as pd
 import geopandas as gpd
+import random
 import networkx as nx
-from shapely.geometry import Point
+from shapely.geometry import Point, MultiLineString
 
 
 def load_data(file_path):
@@ -15,88 +16,126 @@ def load_data(file_path):
             df[col].fillna(df['day_avg'], inplace=True)
     return df
 
-def build_network_graph(geojson_path):
-    """Build a network graph from the geojson file."""
-    geo_df = gpd.read_file(geojson_path)
+def round_coords(coord):
+    return (round(coord[0], 6), round(coord[1], 6))
+
+def build_network_graph(geo_df):
+    """Build a network graph from a GeoDataFrame."""
     G = nx.DiGraph()
     for index, row in geo_df.iterrows():
-        # Assuming each MultiLineString's first point is the start and last is the end
-        if isinstance(row['geometry'], Point):
-            continue
-        start_point = row['geometry'][0].coords[0]
-        end_point = row['geometry'][-1].coords[-1]
+        geom = row['geometry']
         segment_id = row['segment_id']
-        G.add_node(start_point)
-        G.add_node(end_point)
-        G.add_edge(start_point, end_point, segment_id=segment_id)
+        if isinstance(geom, Point):
+            continue
+        if isinstance(geom, MultiLineString):
+            for line in geom.geoms:
+                start_point = round_coords(line.coords[0])
+                end_point = round_coords(line.coords[-1])
+                G.add_edge(start_point, end_point, segment_id=segment_id)
+        else:
+            start_point = round_coords(geom.coords[0])
+            end_point = round_coords(geom.coords[-1])
+            G.add_edge(start_point, end_point, segment_id=segment_id)
     return G
+
+def map_segments_to_edges(geo_df, G):
+    """Map segment IDs to edges in the graph."""
+    segment_to_edge = {}
+    for index, row in geo_df.iterrows():
+        segment_id = row['segment_id']
+        geom = row['geometry']
+        if isinstance(geom, MultiLineString):
+            line = list(geom.geoms)[0]
+        else:
+            line = geom
+        start_point = round_coords(line.coords[0])
+        end_point = round_coords(line.coords[-1])
+        segment_to_edge[segment_id] = (start_point, end_point)
+    return segment_to_edge
+
 
 def define_variables(model, data, G):
     """Define the binary decision variables for the optimization model."""
-    segments = data['segment_id'].unique()
     hours = [f'hour_{h}_avg' for h in range(24)]
-    x_vars = model.addVars(segments, hours, vtype=GRB.BINARY, name="x")
+    x_vars = model.addVars(G.edges(), hours, vtype=GRB.BINARY, name="x")
     return x_vars
 
-def add_objective_function(model, x_vars, data):
+def add_objective_function(model, x_vars, data, segment_to_edge):
     """Add the objective function to minimize total travel time."""
-    objective = gp.quicksum(x_vars[segment, hour] * data.at[segment, hour]
-                            for segment in x_vars.keys()
-                            for hour in x_vars[segment].keys())
+    objective = gp.quicksum(
+        x_vars[edge, hour] * data.loc[data['segment_id'] == segment, hour].values[0]
+        for segment, edge in segment_to_edge.items()
+        for hour in [f'hour_{h}_avg' for h in range(24)]
+        if (edge, hour) in x_vars  # Ensure the key exists in x_vars
+    )
     model.setObjective(objective, GRB.MINIMIZE)
 
-def add_constraints(model, x_vars, data, G):
+
+def add_constraints(model, x_vars, data, G, segment_to_edge):
     """Add constraints to the model based on the network graph."""
-    time_periods = 24  # This should be set according to your problem's requirements
-    origin = 'origin_node_identifier'  # Replace with the actual identifier for the origin node
-    destination = 'destination_node_identifier'  # Replace with the actual identifier for the destination node
-    num_segments = len(data['segment_id'].unique())
-    segment = data['segment_id'].unique()
+    time_periods = [f'hour_{h}_avg' for h in range(24)]
+
+    # Random origin and destination selection
+    nodes = list(G.nodes())
+    origin, destination = random.sample(nodes, 2)
+
     # Connectivity constraints
     for n in G.nodes():
         if n not in [origin, destination]:
-            for t in range(time_periods):
-                model.addConstr(
-                    gp.quicksum(x_vars[i, t] for i, _ in G.in_edges(n)) ==
-                    gp.quicksum(x_vars[j, t] for _, j in G.out_edges(n))
-                )
+            for t in time_periods:
+                in_edges = [segment_to_edge[segment] for segment, edge in segment_to_edge.items() 
+                            if edge[0] == n and (edge, t) in x_vars]
+                out_edges = [segment_to_edge[segment] for segment, edge in segment_to_edge.items() 
+                             if edge[1] == n and (edge, t) in x_vars]
 
-    for t in range(time_periods):
-        model.addConstr(gp.quicksum(x_vars[i, t] for i in G.in_edges(origin)) == 0)
-        model.addConstr(gp.quicksum(x_vars[j, t] for j in G.out_edges(destination)) == 0)
+                model.addConstr(gp.quicksum(x_vars[edge, t] for edge in in_edges) ==
+                                gp.quicksum(x_vars[edge, t] for edge in out_edges))
 
-    # Add path continuity constraints
-    for i in range(num_segments):
-        for t in range(time_periods - 1):
-            model.addConstr(x_vars[i, t] <= gp.quicksum(x_vars[j, t + 1] for j in segment[i]))
+    # Origin-destination constraints
+    for t in time_periods:
+        origin_out_edges = [segment_to_edge[segment] for segment, edge in segment_to_edge.items() 
+                            if edge[0] == origin and (edge, t) in x_vars]
+        destination_in_edges = [segment_to_edge[segment] for segment, edge in segment_to_edge.items() 
+                                if edge[1] == destination and (edge, t) in x_vars]
+        while origin == destination:
+            origin, destination = random.sample(nodes, 2)
+        model.addConstr(gp.quicksum(x_vars[edge, t] for edge in origin_out_edges) == -1)  # Origin has one outgoing edge
+        model.addConstr(gp.quicksum(x_vars[edge, t] for edge in destination_in_edges) == 1)  # Destination has one incoming edge
 
 
 def optimize_route(traffic_data_path, network_data_path):
     """Optimize the route based on traffic and network data."""
     traffic_data = load_data(traffic_data_path)
-    network_graph = build_network_graph(network_data_path)
+    network_data = gpd.read_file(network_data_path)
 
-    # Create a new model
+    # Filter datasets for common segment_ids
+    common_segment_ids = set(traffic_data['segment_id']).intersection(set(network_data['segment_id']))
+    traffic_data = traffic_data[traffic_data['segment_id'].isin(common_segment_ids)]
+    network_data = network_data[network_data['segment_id'].isin(common_segment_ids)]
+
+    network_graph = build_network_graph(network_data)
+    segment_to_edge = map_segments_to_edges(network_data, network_graph)
+    # Create and optimize the model
     model = gp.Model("RouteOptimization")
-
-    # Define variables
     x_vars = define_variables(model, traffic_data, network_graph)
-
-    # Add objective function
-    add_objective_function(model, x_vars, traffic_data)
-
-    # Add constraints
-    add_constraints(model, x_vars, traffic_data, network_graph)
-
-    # Optimize model
+    add_objective_function(model, x_vars, traffic_data, segment_to_edge)
+    add_constraints(model, x_vars, traffic_data, network_graph, segment_to_edge)
     model.optimize()
+    if model.status == GRB.INF_OR_UNBD or model.status == GRB.INFEASIBLE:
+        print("Model is infeasible.")
+        # Perform infeasibility analysis
+        model.computeIIS()
+        model.write("model.ilp")
+        print("Infeasibility report written to 'model.ilp'")
+        return None
 
-    # Extract the solution
-    solution = {}
-    for v in model.getVars():
-        if v.x > 0.5:  # Only consider segments chosen in the solution
-            solution[v.varName] = v.x
-    return solution
+    # Extract and return the solution if feasible
+    if model.status == GRB.OPTIMAL:
+        solution = {v.varName: v.x for v in model.getVars() if v.x > 0.5}
+        return solution
+
+    return None
+
 
 if __name__ == "__main__":
     traffic_data_path = './week_2016-01-11/summary_week_2016-01-11.csv'
